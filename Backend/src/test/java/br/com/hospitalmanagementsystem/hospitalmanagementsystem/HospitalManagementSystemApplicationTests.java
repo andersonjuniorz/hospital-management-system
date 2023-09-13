4bf6e1a721a1b43d422cfdb568c893b67ee631fc
andersonjuniorz <dev.andersonjunior@gmail.com>
2023-09-13 03:16:28 -0300

    adicionando o backend

---
+++ Backend/src/test/java/br/com/hospitalmanagementsystem/hospitalmanagementsystem/HospitalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hospitalmanagementsystem.hospitalmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
